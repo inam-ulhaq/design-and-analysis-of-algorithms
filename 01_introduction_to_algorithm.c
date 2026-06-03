afe6dcf8eda4e8312b5341032b7d56d9dd156fd7
Inam Ul Haq <inamulhaq.engineer@gmail.com>
2026-06-03 23:48:06 +0500

    Introduction_to_algorithms

---
+++ 01_introduction_to_algorithm.c
@@ -0,0 +1,70 @@
+/*
+ Title: Introduction_to_Algorithms
+ Author: Inam Ul haq
+ Description:
+	Algorithm
+		Algorithms is a step by step solution for solving a computational problem.
+	We also define program in the same way i.e
+	
+	Program
+		is a step by step solution to solve a specific problem. Then what's the 
+		difference between them?
+		
+	Difference between Algorithm and Program
+
+	Software development is carried out in two main phases, i.e the design phase 
+	and the implementation phase.
+	In the design phase, we first decide how the software will be developed and what it 
+	is expected to do. After completing this step, we move to the implementation phase, 
+	where the actual coding is done.
+	
+	This process helps us avoid wasting time by writing multiple incorrect or incomplete 
+	programs. Instead, we directly implement a clear and correct solution that meets the 
+	requirements.
+	
+	During the design phase, no specific programming language is used. The design can be 
+	written in any language or environment, and it does not necessarily require a computer. 
+	It can even be done using pen and paper. Since we are not writing programs during this 
+	phase, we write algorithms.
+	
+	An algorithm is a step by step procedure used to solve a problem, while a program is the 
+	implementation of that algorithm using a specific programming language.
+	
+					Algorithms                        | Program
+		written -> During designing phase             | During implementation phase
+		
+		who wrote -> The person who has domain and    | Programmer wrote programs
+		             problem knowledge and can        |
+		             provide solution                 |
+		
+		Language -> Any language can be used to       | Only programming language is used,
+		            design an algorithm as long as    | i.e C, C++, Java, etc.
+		            it can be understood              |
+		
+		hardware and software
+		dependency -> Not dependent on specific       | Dependent on specific hardware
+            		  hardware and software           | and software
+            		  
+----------------------------------------------------------------------------------------------------------------          		  
+            		  
+            	
+	Aspect                  | Priori Analysis                 | Posteriori Testing
+	------------------------| ------------------------------- | -------------------------------
+	What is analyzed        | Time and space complexity       | Actual execution time and
+	                        | theoretically                   | memory usage
+	
+	When analyzed           | Before implementation           | After implementation
+	
+	How it is done          | By logical and mathematical     | By executing the program
+	                    	| analysis                        | on a computer
+	
+	Execution required      | No                              | Yes
+	
+	Hardware dependency     | Independent of hardware         | Dependent on hardware
+	
+	Programming language    | Not dependent                   | Dependent on language and
+	                        |                                 | compiler
+	                           
+	                           
+*/ 
+	
