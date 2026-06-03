25dcb62ed8e8cab486c2425522e0650792c9f34e
Inam Ul Haq <inamulhaq.engineer@gmail.com>
2026-06-03 23:57:52 +0500

    Characteristics_of_algorithm

---
+++ 02_characteristics_of_algorithm.c
@@ -0,0 +1,20 @@
+ Title: Charactaristics of an Algorithm
+ Author: Inam Ul Haq
+ Description:
+	1- Input
+		Algorithm must take sum input either zero input or more.
+	2- Output: 
+		An algorithm must genrate at least one input otherwise algorithm will useless.
+		Some functions or not returning anything but it does somethings.
+	3- Definitness:
+		Every statement have must single and exect one meaning, i.e it must be clear and
+		must be solved, if we can't solve it the computer is also unable to solve that.
+	4- Finiteness:
+		An algorithm must have finite number of statement and it's duration must be terimnated
+		at some point. Some services are contineously runing i.e orical serive countineou untill
+		we stop it manually but algorithm is not a serivce, it must have to terminate at
+		some point.
+	5- Effectiveness:
+		Statements of algorithm must be effective and having some purpose,
+		unnecessary statements are not allowed in algorithm.
+		
